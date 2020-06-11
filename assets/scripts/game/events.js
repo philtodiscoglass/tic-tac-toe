a6619ab7babd88d44344a94d96c6459e13e2e1b1
Phil Todisco <phil@Phils-MBP.fios-router.home>
2020-06-11 16:34:03 -0400

    Game board activity Added x's and o's to the game board Update game function keeps track of cell index value

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -2,18 +2,42 @@
 
 const api = require('./api')
 const ui = require('./ui')
+const store = require('./../store')
 // const getFormFields = require('./../../../lib/get-form-fields')
 
 const onNewGame = function (event) {
   event.preventDefault()
+  currentPlayer = 'X'
+  $('.col-4').text('')
   api.newGame()
     .then(ui.onNewGameSuccess)
     .catch(ui.onNewGameFailure)
 }
 
+let currentPlayer = 'X'
+
 const onUpdateGame = function (event) {
   event.preventDefault()
+  // if (currentPlayer !== 'X' || 'O') {
+  // store.game.cells[$(event.target).data('cell-index')] = currentPlayer
+  // }
+
   console.log($(event.target).data('cell-index'))
+  console.log(store.game.cells)
+
+  if ($(event.target).text() === '') {
+    store.game.cells[$(event.target).data('cell-index')] = currentPlayer
+    $(event.target).text(currentPlayer)
+    if (currentPlayer === 'X') {
+      currentPlayer = 'O'
+    } else {
+      currentPlayer = 'X'
+    }
+  } else {
+    $('#message').show().text('Invalid move')
+    return 'invalid move'
+  }
+
   api.updateGame()
     .then(ui.onUpdateGameSuccess)
     .catch(ui.onUpdateGameFailure)
